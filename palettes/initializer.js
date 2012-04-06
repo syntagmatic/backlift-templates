8b2a9d40ae77dd3e9de7792cc7905cde21aa22b0
Kai Chang <kai.salmon.chang@gmail.com>
2012-04-06 16:04:10 -0700

    added palettes app

---
+++ palettes/initializer.js
@@ -0,0 +1,59 @@
+// let this run once to populate the database with some sample colors
+
+var colors = {
+ 'pastels': [
+    '#66c2a5',
+    '#fc8d62',
+    '#8da0cb',
+    '#e78ac3',
+    '#a6d854'
+  ],
+  'purple green': [
+    '#7b3294',
+    '#c2a5cf',
+    '#f7f7f7',
+    '#a6dba0',
+    '#008837'
+  ],
+  'orange purple': [
+    '#e66101',
+    '#fd8863',
+    '#f7f7f7',
+    '#b2abd2',
+    '#5e3c99'
+  ],
+  'soft reds': [
+    '#fef0d9',
+    '#fdcc8a',
+    '#fc8d59',
+    '#e34a33',
+    '#b30000'
+  ],
+  'teals': [
+    '#f6eff7',
+    '#bdc9e1',
+    '#67a9cf',
+    '#1c9099',
+    '#016c59'
+  ],
+  'grayscale': [
+    '#f7f7f7',
+    '#cccccc',
+    '#969696',
+    '#636363',
+    '#252525'
+  ],
+  'red to blue': [
+    "#d7191c",
+    "#fdae61",
+    "#ffffbf",
+    "#abdda4",
+    "#2b83ba"
+  ]
+}
+_(colors).each(function(v,k) {
+Palettes.create({
+  name: k,
+  colors: v
+})
+});
---
+++ palettes/palettes.js
@@ -0,0 +1,103 @@
+var randomColor = function() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+};
+
+var Palette = Backbone.Model.extend({
+  idAttribute: "_id",
+  defaults: function() {
+    return {
+      name: "Untitled",
+      colors: _.map( _.range(5), randomColor )
+    };
+  },
+});
+
+var defaultPalette = new Palette;
+
+var PaletteList = Backbone.Collection.extend({
+  model: Palette,
+  url: 'backlift/palettes'
+});
+
+var PaletteView = Backbone.View.extend({
+  tagName: "li",
+  template: _.template($('#item-template').html()),
+  events: {
+    'click': 'setPicker'
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  },
+  setPicker: function() {
+    defaultPalette.set({
+      colors: this.model.get("colors")
+    });
+  }
+});
+
+var PickerView = Backbone.View.extend({
+  el: $('#picker'),
+  events: {
+    "click #create-palette": "createPalette",
+    "click #random-palette": "randomPalette"
+  },
+  initialize: function() {
+    var self = this;
+    this.model.bind('change', this.render, this);
+    _.each(_.range(5), function(i) {
+      $('#color-' + i).on('focus', function() {
+        // this strange line sets the cursor to the end of text input when focused
+        $(this).val( $(this).val() );
+      });
+      $('#swatch-'+ i).click(function() {
+        $('#color-' + i).focus();
+      });
+    });
+    $('#color-pickers input').each(function(i) {
+      $(this).on('keyup', function() {
+        var colors = _.clone( self.model.get('colors') );
+        colors[i] = $(this).val();
+        self.model.set('colors', colors, {silent: true});
+        $('#swatch-' + i).animate({background: colors[i] }, 400)
+      });
+    });
+    this.render();
+  },
+  render: function() {
+    var colors = this.model.get('colors');
+    _(colors).each(function(d,i) {
+      $('#swatch-' + i).animate({background: d}, 640);
+      $('#color-' + i).val(d);
+    });
+  },
+  createPalette: function() {
+    Palettes.create(this.model.toJSON());
+    window.scrollTo( window.scrollX, 0);
+  },
+  randomPalette: function() {
+    this.model.set({colors: _.map( _.range(5), randomColor ) });
+  }
+});
+
+var PaletteListView = Backbone.View.extend({
+  el: $("#palette-list"),
+  initialize: function() {
+    Palettes.bind('add', this.addOne, this);
+    Palettes.bind('reset', this.addAll, this);
+  },
+  addOne: function(palette) {
+    var view = new PaletteView({model: palette});
+    $("#palette-list").prepend(view.render().el);
+  },
+  addAll: function() {
+    Palettes.each(this.addOne);
+  },
+});
+
+var Palettes = new PaletteList;
+
+var pickerView = new PickerView({ model: defaultPalette });
+var palettesView = new PaletteListView;
+
+Palettes.fetch();
